38acacd043b7771ec90d76ac47dcc301ca3cd214
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-01-25 16:57:59 +0700

    knexfile: update on production

--- database/knexfile.js
+++ database/knexfile.js
@@ -27,10 +27,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
     },
     seeds: {
-      directory: './seeds'
+      directory: './database/seeds'
     }
   }
 };
